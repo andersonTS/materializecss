62f1c5cc11727f3d98b55b38dd3268a2ce6d752e
ihor <ihorts18@gmail.com>
2021-08-30 13:22:27 +0300

    Added user info and footer

--- js/script.js
+++ js/script.js
@@ -2,5 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, {});
   });
-    
+  $(document).ready(function() {
+    $('input#input_text, textarea#textarea2').characterCounter();
+  });
    
